dcbb7694a0bdce2f43cc2358bd17d2a0f388325e
Juan Luis Serrano <jlserrano@alerce-group.com>
2023-06-12 21:21:10 +0200

    Detalle pokemon

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
 const Pokemon = ({ data }) => {
     console.log(data)
     return (
-        <p>lala</p>
+        <div>
+            <h1>{data.name} numero #{data.id}</h1>
+            <Image src={data.sprites.front_default} width={400} height={400} />
+            <Link href="/">Volver al inicio</Link>
+        </div>
     )
 }
 
