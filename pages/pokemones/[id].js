d0db1557658c0c40ee88d19385ee1e6f114f1309
Juan Luis Serrano <jlserrano@alerce-group.com>
2023-06-13 15:33:02 +0200

    getStaticPaths

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,9 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 const Pokemon = ({ data }) => {
-    console.log(data)
+    const router = useRouter()
+    console.log(router)
+
+    // if (router.isFallback) {
+    //     return <p>Cargando</p>
+    // }
+
     return (
         <div>
             <h1>{data.name} numero #{data.id}</h1>
@@ -15,9 +22,28 @@ const Pokemon = ({ data }) => {
 
 export default Pokemon
 
-export const getServerSideProps = async ({ params }) => {
+export const getStaticProps = async ({ params }) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
     const data = await response.json()
 
     return { props: { data } }
 }
+
+export const getStaticPaths = async () => {
+    const paths = [
+        { params: { id: '1' }},
+        { params: { id: '2' }},
+    ]
+    return {
+        paths,
+        // fallback: true,
+        fallback: 'blocking',
+    }
+}
+
+// export const getServerSideProps = async ({ params }) => {
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+//     const data = await response.json()
+
+//     return { props: { data } }
+// }
